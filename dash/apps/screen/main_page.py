c1f6f0a21ce80dc56b6559d89721bc2b238ab049
Rafaela Pinter <rpinter811@gmail.com>
2022-06-17 15:55:40 -0300

    centering login button

--- dash/apps/screen/main_page.py
+++ dash/apps/screen/main_page.py
@@ -4,16 +4,27 @@ import dash_bootstrap_components as dbc
 layout = html.Div(
     [
       dbc.Col(
-                    [
-                        html.P('\n'),
-                        dbc.Button(
-                            "Login with Spotify", 
-                            color="success", 
-                            id='pl-login_button',
-                            className="me-1",
-                            n_clicks=0
-                            ),
-                    ],
-                )
-    ]
+            [
+                html.P('\n'),
+                dbc.Button(
+                    "Login with Spotify", 
+                    color="success", 
+                    id='pl-login_button',
+                    className="me-1",
+                    size="lg",
+                    n_clicks=0,
+                ),
+            ],
+            style={
+                # "margin-top":"50px",
+                "margin-left": "auto",
+                "margin-right": "auto",
+            },
+            width={"size": 3}
+        )
+    ],
+    style={
+        # "margin-top":"50px",
+        "margin": "auto",
+    },
 )
